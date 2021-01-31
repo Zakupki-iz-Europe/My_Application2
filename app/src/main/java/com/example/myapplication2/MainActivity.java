38d0c3b2f0d699e48ff7ef6180c907a009a95d63
VAS6150B <porno_mail@bk.ru>
2021-01-31 20:53:32 +0300

    добавил чекбоксы

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.provider.MediaStore;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,28 +16,102 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import io.realm.OrderedCollectionChangeSet;
+import org.bson.types.ObjectId;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ToggleButton;
+
+import io.realm.OrderedRealmCollectionChangeListener;
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
+import io.realm.mongodb.sync.SyncConfiguration;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import com.example.myapplication2.Task;
+import com.example.myapplication2.TaskStatus;
+
+import static java.lang.Boolean.TRUE;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String FILE_NAME = "content.txt";
+    Realm uiThreadRealm;
+    App app;
+    RadioButton rbZN, rbZNPP;
+    EditText et_zakaz_naryad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        et_zakaz_naryad = findViewById(R.id.zakaz_naryad);
+        rbZN = findViewById(R.id.radioButton);
+        rbZNPP = findViewById(R.id.radioButton2);
+        rbZN.setChecked(TRUE); // программно устанавливаем статус у переключателя radioButton
+
         super.onCreate(savedInstanceState);
+
+        et_zakaz_naryad.setOnKeyListener(new View.OnKeyListener()
+                                  {
+                                      public boolean onKey(View v, int keyCode, KeyEvent event)
+                                      {
+                                          if(event.getAction() == KeyEvent.ACTION_DOWN &&
+                                                  (keyCode == KeyEvent.KEYCODE_ENTER))
+                                          {
+                                              // сохраняем текст, введённый до нажатия Enter в переменную
+                                              String strCatName = et_zakaz_naryad.getText().toString();
+                                              return true;
+                                          }
+                                          return false;
+                                      }
+                                  }
+        );
+
+
+        Realm.init(this); // context, usually an Activity or Application
+        String appID = "zakaz-naryad-dspnq";
+        app = new App(new AppConfiguration.Builder(appID)
+                .build());
+        Credentials credentials = Credentials.anonymous();
+        app.loginAsync(credentials, result -> {
+            if (result.isSuccess()) {
+                Log.v("QUICKSTART", "Successfully authenticated anonymously.");
+                User user = app.currentUser();
+                String partitionValue = "My Project";
+                SyncConfiguration config = new SyncConfiguration.Builder(
+                        user,
+                        partitionValue)
+                        .build();
+                uiThreadRealm = Realm.getInstance(config);
+                addChangeListenerToRealm(uiThreadRealm);
+                FutureTask<String> task = new FutureTask(new BackgroundQuickStart(app.currentUser()), "test");
+                ExecutorService executorService = Executors.newFixedThreadPool(2);
+                executorService.execute(task);
+            } else {
+                Log.e("QUICKSTART", "Failed to log in. Error: " + result.getError());
+            }
+        });
         setContentView(R.layout.activity_main);
     }
 
 
     // сохранение файла
-    public void saveText(View view){
+    public void saveText(View view) {
 
         FileOutputStream fos = null;
         try {
-            EditText textBox = (EditText) findViewById(R.id.zakaz_naryad);
-            String text = textBox.getText().toString() + ' ';
-
+            EditText textBox;
+            String text = et_zakaz_naryad.getText().toString() + ' ';
+            if (rbZN.isChecked())
+                text = rbZN.getText() + text;
+            else text = rbZNPP.getText() + text;
             textBox = (EditText) findViewById(R.id.normochasy);
             text = text + textBox.getText().toString() + ' ';
 
@@ -46,24 +123,22 @@ public class MainActivity extends AppCompatActivity {
             fos = openFileOutput(FILE_NAME, MODE_APPEND);
             fos.write(text.getBytes());
             Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
-        }
-        catch(IOException ex) {
+        } catch (IOException ex) {
 
             Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-        finally{
-            try{
-                if(fos!=null)
+        } finally {
+            try {
+                if (fos != null)
                     fos.close();
-            }
-            catch(IOException ex){
+            } catch (IOException ex) {
 
                 Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
     }
+
     // открытие файла
-    public void openText(View view){
+    public void openText(View view) {
 
         FileInputStream fin = null;
         TextView textView = (TextView) findViewById(R.id.open_text);
@@ -71,49 +146,129 @@ public class MainActivity extends AppCompatActivity {
             fin = openFileInput(FILE_NAME);
             byte[] bytes = new byte[fin.available()];
             fin.read(bytes);
-            String text = new String (bytes);
+            String text = new String(bytes);
             textView.setText(text);
-        }
-        catch(IOException ex) {
+        } catch (IOException ex) {
 
             Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-        finally{
+        } finally {
 
-            try{
-                if(fin!=null)
+            try {
+                if (fin != null)
                     fin.close();
-            }
-            catch(IOException ex){
+            } catch (IOException ex) {
 
                 Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
     }
+
     // удаление файла
-    public void clearText(View view){
+    public void clearText(View view) {
         FileInputStream fin = null;
         TextView textView = (TextView) findViewById(R.id.open_text);
         try {
             deleteFile(FILE_NAME);
             textView.setText(null);
             clearField();
-        } finally{
+        } finally {
 
-            try{
-                if(fin!=null)
+            try {
+                if (fin != null)
                     fin.close();
-            }
-            catch(IOException ex){
+            } catch (IOException ex) {
 
                 Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
     }
+
     // очистка полей ввода
     public void clearField() {
-        ((EditText) findViewById(R.id.zakaz_naryad)).setText("");
+        et_zakaz_naryad.setText("");
         ((EditText) findViewById(R.id.normochasy)).setText("");
         ((EditText) findViewById(R.id.data)).setText("");
     }
+
+    private void addChangeListenerToRealm(Realm realm) {
+        // all tasks in the realm
+        RealmResults<Task> tasks = uiThreadRealm.where(Task.class).findAllAsync();
+        tasks.addChangeListener(new OrderedRealmCollectionChangeListener<RealmResults<Task>>() {
+            @Override
+            public void onChange(RealmResults<Task> collection, OrderedCollectionChangeSet changeSet) {
+                // process deletions in reverse order if maintaining parallel data structures so indices don't change as you iterate
+                OrderedCollectionChangeSet.Range[] deletions = changeSet.getDeletionRanges();
+                for (OrderedCollectionChangeSet.Range range : deletions) {
+                    Log.v("QUICKSTART", "Deleted range: " + range.startIndex + " to " + (range.startIndex + range.length - 1));
+                }
+                OrderedCollectionChangeSet.Range[] insertions = changeSet.getInsertionRanges();
+                for (OrderedCollectionChangeSet.Range range : insertions) {
+                    Log.v("QUICKSTART", "Inserted range: " + range.startIndex + " to " + (range.startIndex + range.length - 1));
+                }
+                OrderedCollectionChangeSet.Range[] modifications = changeSet.getChangeRanges();
+                for (OrderedCollectionChangeSet.Range range : modifications) {
+                    Log.v("QUICKSTART", "Updated range: " + range.startIndex + " to " + (range.startIndex + range.length - 1));
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // the ui thread realm uses asynchronous transactions, so we can only safely close the realm
+        // when the activity ends and we can safely assume that those transactions have completed
+        uiThreadRealm.close();
+        app.currentUser().logOutAsync(result -> {
+            if (result.isSuccess()) {
+                Log.v("QUICKSTART", "Successfully logged out.");
+            } else {
+                Log.e("QUICKSTART", "Failed to log out, error: " + result.getError());
+            }
+        });
+    }
+
+    public class BackgroundQuickStart implements Runnable {
+        User user;
+
+        public BackgroundQuickStart(User user) {
+            this.user = user;
+        }
+
+        @Override
+        public void run() {
+            String partitionValue = "My Project";
+            SyncConfiguration config = new SyncConfiguration.Builder(
+                    user,
+                    partitionValue)
+                    .build();
+            Realm backgroundThreadRealm = Realm.getInstance(config);
+            Task task = new Task("New Task");
+            backgroundThreadRealm.executeTransaction(transactionRealm -> {
+                transactionRealm.insert(task);
+            });
+            // all tasks in the realm
+            RealmResults<Task> tasks = backgroundThreadRealm.where(Task.class).findAll();
+            // you can also filter a collection
+            RealmResults<Task> tasksThatBeginWithN = tasks.where().beginsWith("name", "N").findAll();
+            RealmResults<Task> openTasks = tasks.where().equalTo("status", TaskStatus.Open.name()).findAll();
+            Task otherTask = tasks.get(0);
+            // all modifications to a realm must happen inside of a write block
+            backgroundThreadRealm.executeTransaction(transactionRealm -> {
+                Task innerOtherTask = transactionRealm.where(Task.class).equalTo("_id", otherTask.get_id()).findFirst();
+                innerOtherTask.setStatus(TaskStatus.Complete);
+            });
+            Task yetAnotherTask = tasks.get(0);
+            ObjectId yetAnotherTaskId = yetAnotherTask.get_id();
+            // all modifications to a realm must happen inside of a write block
+            backgroundThreadRealm.executeTransaction(transactionRealm -> {
+                Task innerYetAnotherTask = transactionRealm.where(Task.class).equalTo("_id", yetAnotherTaskId).findFirst();
+                innerYetAnotherTask.deleteFromRealm();
+            });
+            // because this background thread uses synchronous realm transactions, at this point all
+            // transactions have completed and we can safely close the realm
+            backgroundThreadRealm.close();
+        }
+    }
+
 }
