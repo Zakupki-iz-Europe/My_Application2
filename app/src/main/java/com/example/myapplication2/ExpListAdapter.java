a082ceae6147f266ee14329db2f605884a00a023
VAS6150B <porno_mail@bk.ru>
2021-03-14 21:01:58 +0300

    всё работает, нужно сделать удаление при долгом нажатии и редактирование при коротком и сумму часов за день в справа от даты

--- app/src/main/java/com/example/myapplication2/ExpListAdapter.java
+++ app/src/main/java/com/example/myapplication2/ExpListAdapter.java
@@ -1,8 +1,10 @@
 package com.example.myapplication2;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,11 +14,18 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.RequiresApi;
+
 import com.example.myapplication2.sqlite.DatabaseHelper;
 import com.example.myapplication2.sqlite.Note;
 
 import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -26,62 +35,27 @@ public class ExpListAdapter extends BaseExpandableListAdapter {
     private final ArrayList<ArrayList<Note>> mGroups = new ArrayList<ArrayList<Note>>();
     private ArrayList<String> mDays;
     private final Context mContext;
-    private final SQLiteDatabase mydb;
     private final String LOG_TAG = "List";
-    private  final ArrayList<Note> notes = new ArrayList<Note>();
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public ExpListAdapter (Context context, DatabaseHelper db){
-        List<Note> notes = db.getAllNotes();
-        mydb = db.getReadableDatabase();
         mContext = context;
-        String str = "";
-        int index = 0;
-
         mDays = db.distinct(Note.COLUMN_DATE);
+
         if (mDays != null){
+// я не знаю как, но это работает!!! надо срочно разбираться с лямбда-функциями
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            Collections.sort(mDays, (s1, s2) -> LocalDate.parse(s1, formatter).
+                    compareTo(LocalDate.parse(s2, formatter)));
+
             for(String data:mDays){
                mGroups.add(db.where(Note.COLUMN_DATE, data));
             }
-
-//        String selectQuery = "SELECT  DISTINCT " +  Note.COLUMN_DATE + " FROM " + Note.TABLE_NAME;
-//
-//        Cursor cursor = mydb.rawQuery(selectQuery, null);
-//
-//        // looping through all rows and adding to list
-//        if (cursor.moveToFirst()) {
-//            do { // дергаем каждый день
-////                Map<String,Double> map = new HashMap<>();
-//                str = cursor.getString(cursor.getColumnIndex(Note.COLUMN_DATE));
-//                mDays.add(str);
-//                Log.d(LOG_TAG, "-------" + str);
-////               а теперь определим сколько заказов было за этот день
-//                selectQuery = "SELECT " +  Note.COLUMN_ZAK + "," + Note.COLUMN_CHAS +
-//                        " FROM " + Note.TABLE_NAME + " WHERE " + Note.COLUMN_DATE +
-//                        " = '" + str + "'";
-//                Cursor curNotes = mydb.rawQuery(selectQuery, null);
-//                if (curNotes.moveToFirst()) {
-//                    do {
-//
-//                        map.put(curNotes.getString(curNotes.getColumnIndex(Note.COLUMN_ZAK)),
-//                                curNotes.getDouble(curNotes.getColumnIndex(Note.COLUMN_CHAS)));
-//                        Log.d(LOG_TAG, curNotes.getString(curNotes.getColumnIndex(Note.COLUMN_ZAK)) + "-------" + curNotes.getDouble(curNotes.getColumnIndex(Note.COLUMN_CHAS)));
-//
-//                    } while (curNotes.moveToNext());
-//                    Log.d(LOG_TAG, map + "-----------map----------");
-//                }
-//                mGroups.add(map);
-//                index++;
-//                curNotes.close();
-//            } while (cursor.moveToNext());
         }
         else {
-//            Map<String,Double> map = new HashMap<>();
             mDays.add("Нет даты");
-//            map.put("Нет заказов", (double) 0);
             mGroups.add(new ArrayList<>());}
-//        cursor.close();
         Log.d(LOG_TAG, mGroups + "-------" );
-
     }
 
 
@@ -143,6 +117,7 @@ public class ExpListAdapter extends BaseExpandableListAdapter {
 
     }
 
+    @SuppressLint("DefaultLocale")
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild,
                              View convertView, ViewGroup parent) {
@@ -159,7 +134,8 @@ public class ExpListAdapter extends BaseExpandableListAdapter {
                 Double chasy = mGroups.get(groupPosition).get(childPosition).getChas();
 
                 textChild.setText(zak_nar);
-                zak_nar = chasy.toString();
+
+                zak_nar = String.format("%.2f",chasy);
                 textChasy.setText(zak_nar);
         }
 
